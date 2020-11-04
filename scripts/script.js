2bed721014644707e34519d0c2d6e7816707ec0d
TessaSchijff <70887234+TessaSchijff@users.noreply.github.com>
2020-11-04 18:48:43 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // versie 1      -     Tessa Schijff      -      500809765      -       Klas 208b
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
